b7d3a2fefa656b3686b10d8032453e4cf39ef82e
blank <blank>
2024-08-25 13:29:37 +0300

    - Added test for UTF-8 encoded file - Added test for .exe file

---
+++ test/base64_file_test.cpp
@@ -0,0 +1,35 @@
+#include <gtest/gtest.h>
+
+#include "../include/base64.hpp"
+#include <string>
+#include <fstream>
+
+TEST(base64_encode_decode, Exe_File)
+{
+    std::ifstream t("resources/test.exe");
+    std::stringstream buffer;
+    buffer << t.rdbuf();
+    std::string const expected{buffer.str()};
+    std::string const actual{base64::decode({base64::encode({buffer.str()})})};
+    ASSERT_EQ(expected, actual);
+}   
+
+TEST(base64_encode_decode, UTF8_File)
+{
+    std::ifstream t("resources/UTF-8.txt");
+    std::stringstream buffer;
+    buffer << t.rdbuf();
+    std::string const expected{buffer.str()};
+    std::string const actual{base64::decode({base64::encode({buffer.str()})})};
+    ASSERT_EQ(expected, actual);
+}   
+
+TEST(base64_encode_decode, CP1251_File)
+{
+    std::ifstream t("resources/cp-1251.txt");
+    std::stringstream buffer;
+    buffer << t.rdbuf();
+    std::string const expected{buffer.str()};
+    std::string const actual{base64::decode({base64::encode({buffer.str()})})};
+    ASSERT_EQ(expected, actual);
+}   
