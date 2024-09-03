81bcfe8de9b9627b6e51b1e5dbc2560090207070
Dmitriy <punov.dv@phystech.edu>
2024-09-03 15:28:26 +0300

    Decompose of encode and decode function

--- include/base64.hpp
+++ include/base64.hpp
@@ -10,8 +10,11 @@
 namespace base64
 {
 
+namespace detail
+{
+
 // Encoding table for base64 converter //
-std::array<char, 64> constexpr encodeTable
+constexpr std::array<char, 64> encodeTable
 {
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 
     'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
@@ -24,7 +27,7 @@ std::array<char, 64> constexpr encodeTable
 };
 
 // Decoding table for base 64 converter //
-std::array<std::uint8_t, 256> constexpr decodeTable
+constexpr std::array<std::uint8_t, 256> decodeTable
 {
     0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 0x64,
     0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 
@@ -44,109 +47,65 @@ std::array<std::uint8_t, 256> constexpr decodeTable
     0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 0x64, 0x64
 };
 
-// ENCODING //
-inline std::string encode(std::string_view inputString)
+inline void encodingMainPartString(std::string_view inputString, std::string &outputString)
 {
-
-    // Size of input string //
-    const size_t inputDataSize = inputString.size();
-
-    // Calculate place for encoded string //
-    const size_t sizeEncoded = (inputDataSize + 2) / 3 * 4;
-
-    // Reserve place for encoded string
-    std::string encodedString;
-    encodedString.reserve(sizeEncoded);
-
-    // Storing pointer to currently encoded symbol in input string //
     const uint8_t *inputIterator = reinterpret_cast<const uint8_t *>(&inputString[0]);
 
-    // Iterate over blocks of 3 until the end of the input string //
-    for (size_t i = inputDataSize / 3; i; --i)
+    for (size_t i = 0; i < inputString.size() / 3; i++)
     {
-
         const uint8_t symbol1 = *inputIterator++;
         const uint8_t symbol2 = *inputIterator++;
         const uint8_t symbol3 = *inputIterator++;
 
         std::uint32_t const concatenatedBits = (symbol1 << 16) | (symbol2 << 8) | symbol3;
 
-        encodedString.push_back(encodeTable[(concatenatedBits >> 18) & 0b0011'1111]);
-        encodedString.push_back(encodeTable[(concatenatedBits >> 12) & 0b0011'1111]);
-        encodedString.push_back(encodeTable[(concatenatedBits >> 6) & 0b0011'1111]);
-        encodedString.push_back(encodeTable[concatenatedBits & 0b0011'1111]);
+        outputString.push_back(encodeTable[(concatenatedBits >> 18) & 0b0011'1111]);
+        outputString.push_back(encodeTable[(concatenatedBits >> 12) & 0b0011'1111]);
+        outputString.push_back(encodeTable[(concatenatedBits >> 6) & 0b0011'1111]);
+        outputString.push_back(encodeTable[concatenatedBits & 0b0011'1111]);
     }
+}
 
-    // Adding special padding characters //
-    switch (inputDataSize % 3)
+inline void encodingAddPaddingChar(std::string_view inputString, std::string &outputString)
+{
+    switch (inputString.size() % 3)
     {
     case 0:
     {
-
         break;
     }
     case 1:
     {
-
-        const uint8_t sym = *inputIterator++;
-        encodedString.push_back(encodeTable[(sym >> 2) & 0b0011'1111]);
-        encodedString.push_back(encodeTable[(sym << 4) & 0b0011'1111]);
-        encodedString.push_back('=');
-        encodedString.push_back('=');
+        const uint8_t sym = inputString.back();
+        outputString.push_back(encodeTable[(sym >> 2) & 0b0011'1111]);
+        outputString.push_back(encodeTable[(sym << 4) & 0b0011'1111]);
+        outputString.push_back('=');
+        outputString.push_back('=');
         break;
     }
     case 2:
     {
-
-        const uint8_t symbol1 = *inputIterator++;
-        const uint8_t symbol2 = *inputIterator++;
-        encodedString.push_back(encodeTable[(symbol1 >> 2) & 0b0011'1111]);
-        encodedString.push_back(encodeTable[(symbol1 << 4 | symbol2 >> 4) & 0b0011'1111]);
-        encodedString.push_back(encodeTable[(symbol2 << 2) & 0b0011'1111]);
-        encodedString.push_back('=');
+        const uint8_t symbol1 = inputString.at(inputString.size()-2);
+        const uint8_t symbol2 = inputString.back();
+        outputString.push_back(encodeTable[(symbol1 >> 2) & 0b0011'1111]);
+        outputString.push_back(encodeTable[(symbol1 << 4 | symbol2 >> 4) & 0b0011'1111]);
+        outputString.push_back(encodeTable[(symbol2 << 2) & 0b0011'1111]);
+        outputString.push_back('=');
         break;
     }
     }
-
-    return encodedString;
 }
 
-inline std::string decode(std::string_view inputString)
+inline void decodingMainPartString(std::string_view inputString, int numPadding, std::string &outputString)
 {
-
-    if (inputString.empty())
-    {
-        return std::string{};
-    }
-
-    // Size of input string //
-    const size_t inputDataSize = inputString.size();
-
-    // Ammount of paddings in input string //
-    const size_t numPadding = std::count(inputString.rbegin(), inputString.rbegin() + 4, '=');
-    const size_t numNewLine = std::count(inputString.begin(), inputString.end(), '\n');
-    const size_t numReturn = std::count(inputString.begin(), inputString.end(), '\r');
-
-    // Check, if ammount of padding symbols is correct (< 2) //
-    if (numPadding > 2)
-    {
-        throw std::runtime_error{
-            "Invalid base64 string. Couldn't resolve ammount of paddinngs."};
-    }
-
-    // Calculate and reserve place for decoded string //
-    size_t sizeDecoded = (inputDataSize / 4 * 3) - numPadding;
-    std::string decodedString;
-    decodedString.reserve(sizeDecoded);
-
-    // Storing pointer to currently decoded symbol in input string //
     const uint8_t *inputIterator = reinterpret_cast<const uint8_t *>(&inputString[0]);    
 
     uint8_t currentBlock[4];
-    int counterBlock = 0;
+    size_t counterBlock = 0;
 
-    // Iterate over blocks of 4 until the end of the input string //
-    for (size_t i = 0; i < inputDataSize - (numPadding!=0)*4; i++)
+    size_t blocksAmmount = inputString.size() - (numPadding!=0)*4;
+
+    for (size_t i = 0; i < blocksAmmount; i++)
     {   
         currentBlock[counterBlock] = *inputIterator++;
         if (currentBlock[counterBlock] != '\n' && currentBlock[counterBlock]!='\r') counterBlock++;
@@ -154,14 +113,16 @@ inline std::string decode(std::string_view inputString)
         {
         std::uint32_t const concatenatedBits =
             (decodeTable[currentBlock[0]] << 18) | (decodeTable[currentBlock[1]] << 12) | (decodeTable[currentBlock[2]] << 6) | decodeTable[currentBlock[3]];
-        decodedString.push_back((concatenatedBits >> 16) & 0b1111'1111);
-        decodedString.push_back((concatenatedBits >> 8) & 0b1111'1111);
-        decodedString.push_back(concatenatedBits & 0b1111'1111);
+        outputString.push_back((concatenatedBits >> 16) & 0b1111'1111);
+        outputString.push_back((concatenatedBits >> 8) & 0b1111'1111);
+        outputString.push_back(concatenatedBits & 0b1111'1111);
         counterBlock = 0;
         }
     }
+}
 
-    // Work with end of the base64 encoded string //
+inline void decodingPaddingChar(std::string_view inputString, int numPadding, std::string &outputString)
+{
     switch (numPadding)
     {
     case 0:
@@ -170,32 +131,77 @@ inline std::string decode(std::string_view inputString)
     }
     case 1:
     {
-        const uint8_t symbol1 = *inputIterator++;
-        const uint8_t symbol2 = *inputIterator++;
-        const uint8_t symbol3 = *inputIterator++;
+        const uint8_t symbol1 = inputString.at(inputString.size()-4);
+        const uint8_t symbol2 = inputString.at(inputString.size()-3);
+        const uint8_t symbol3 = inputString.at(inputString.size()-2);
 
         std::uint32_t const concatenatedBits =
             (decodeTable[symbol1] << 18) | (decodeTable[symbol2] << 12) | (decodeTable[symbol3] << 6) | 0x64;
 
-        decodedString.push_back((concatenatedBits >> 16) & 0b1111'1111);
-        decodedString.push_back((concatenatedBits >> 8) & 0b1111'1111);
+        outputString.push_back((concatenatedBits >> 16) & 0b1111'1111);
+        outputString.push_back((concatenatedBits >> 8) & 0b1111'1111);
 
         break;
     }
     case 2:
     {
-        const uint8_t symbol1 = *inputIterator++;
-        const uint8_t symbol2 = *inputIterator++;
+        const uint8_t symbol1 = inputString.at(inputString.size()-4);
+        const uint8_t symbol2 = inputString.at(inputString.size()-3);
 
         std::uint32_t const concatenatedBits =
             (decodeTable[symbol1] << 18) | (decodeTable[symbol2] << 12) | 0x64 | 0x64;
 
-        decodedString.push_back((concatenatedBits >> 16) & 0b1111'1111);
+        outputString.push_back((concatenatedBits >> 16) & 0b1111'1111);
 
         break;
     }
     }
+}
+} // namespace detail
+
+inline std::string encode(std::string_view inputString)
+{
+    const size_t inputDataSize = inputString.size();
+
+    const size_t sizeEncoded = (inputDataSize + 2) / 3 * 4;
+
+    std::string encodedString;
+    encodedString.reserve(sizeEncoded);
+
+    detail::encodingMainPartString(inputString, encodedString);
+
+    detail::encodingAddPaddingChar(inputString, encodedString);
+    
+    return encodedString;
+}
+
+
+
+inline std::string decode(std::string_view inputString)
+{
+    if (inputString.empty())
+    {
+        return std::string{};
+    }
+
+    const size_t inputDataSize = inputString.size();
+    const size_t numPadding = std::count(inputString.rbegin(), inputString.rbegin() + 4, '=');
+
+    if (numPadding > 2)
+    {
+        throw std::runtime_error{
+            "Invalid base64 string. Couldn't resolve ammount of paddinngs."};
+    }
+
+    const size_t sizeDecoded = (inputDataSize / 4 * 3) - numPadding;
+
+    std::string decodedString;
+    decodedString.reserve(sizeDecoded);
+
+    detail::decodingMainPartString(inputString, numPadding, decodedString);
+    detail::decodingPaddingChar(inputString, numPadding, decodedString);
 
     return decodedString;
 }
+
 } // namespace base64
